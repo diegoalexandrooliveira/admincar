2c165e57b28d00b484caab394e1c781401b490b5
diegoalexandrooliveira <diegoalexandrooliveira@gmail.com>
2019-06-29 12:40:19 -0300

    commit

--- start-api.sh
+++ start-api.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="/home/diego/workspace/admincar/api/admincar-198704-d13a68d38c0b.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/diego/workspace/admincar/api/PerimetralVeiculos-1910aa9d67e0.json"
 cd /home/diego/workspace/admincar/api
 npm start
